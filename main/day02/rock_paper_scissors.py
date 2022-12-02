0eb517b19946ea6ff3ddec8e82753e16949971f2
Ben Watson <benwatson528@gmail.com>
2022-12-02 12:21:05 +0000

    Using hashmap

--- main/day02/rock_paper_scissors.py
+++ main/day02/rock_paper_scissors.py
@@ -1,6 +1,6 @@
-LOSERS = {('A', 'Z'), ('B', 'X'), ('C', 'Y')}
-WINNERS = {('A', 'Y'), ('B', 'Z'), ('C', 'X')}
-DRAWERS = {('A', 'X'), ('B', 'Y'), ('C', 'Z')}
+LOSERS = {'A': 'Z', 'B': 'X', 'C': 'Y'}
+WINNERS = {'A': 'Y', 'B': 'Z', 'C': 'X'}
+DRAWERS = {'A': 'X', 'B': 'Y', 'C': 'Z'}
 
 
 def solve_move(moves) -> int:
@@ -8,26 +8,26 @@ def solve_move(moves) -> int:
 
 
 def solve_outcome(moves) -> int:
-    updated_moves = list(map(find_move, moves))
+    updated_moves = map(find_move, moves)
     return sum(map(score, updated_moves))
 
 
 def find_move(move):
     opponent, outcome = move
     if outcome == 'X':
-        return opponent, dict(LOSERS)[opponent]
+        return opponent, LOSERS[opponent]
     elif outcome == 'Y':
-        return opponent, dict(DRAWERS)[opponent]
+        return opponent, DRAWERS[opponent]
     else:
-        return opponent, dict(WINNERS)[opponent]
+        return opponent, WINNERS[opponent]
 
 
 def score(move):
     opponent, player = move
     shape_score = ord(player) - 87
-    outcome_score = 3
-    if (opponent, player) in LOSERS:
-        outcome_score = 0
-    elif (opponent, player) in WINNERS:
-        outcome_score = 6
-    return outcome_score + shape_score
+    if LOSERS[opponent] == player:
+        return shape_score
+    elif DRAWERS[opponent] == player:
+        return 3 + shape_score
+    else:
+        return 6 + shape_score
