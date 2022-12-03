b705c80c616eb69b48a53dd190796eee5fa72570
Ben Watson <benwatson528@gmail.com>
2022-12-03 07:12:24 +0000

    Day 3 part 2

--- main/day03/rucksack_reorganization.py
+++ main/day03/rucksack_reorganization.py
@@ -1,10 +1,25 @@
-def solve(rucksacks) -> int:
+def solve_priority(rucksacks) -> int:
     total = 0
     for rucksack in rucksacks:
-        priorities = transform_priorities(rucksack[0]), transform_priorities(rucksack[1])
+        divider = len(rucksack) // 2
+        compartments = rucksack[:divider], rucksack[divider:]
+        priorities = transform_priorities(compartments[0]), transform_priorities(compartments[1])
         total += sum(set(priorities[0]).intersection(set(priorities[1])))
+    return total
+
+
+def solve_badges(rucksacks) -> int:
+    total = 0
+    for group in chunks(rucksacks, 3):
+        common = set(group[0]).intersection(set(group[1])).intersection(set(group[2]))
+        total += transform_priorities(common.pop())[0]
     return total
 
 
 def transform_priorities(compartment):
     return [ord(item) - 96 if item.lower() == item else ord(item) - 38 for item in compartment]
+
+
+def chunks(l, n):
+    for i in range(0, len(l), n):
+        yield l[i:i + n]
--- tests/day03/test_rucksack_reorganization.py
+++ tests/day03/test_rucksack_reorganization.py
@@ -1,22 +1,28 @@
 import os
 from pathlib import Path
 
-from main.day03.rucksack_reorganization import solve
+from main.day03.rucksack_reorganization import solve_priority, solve_badges
 
 
-def test_simple():
-    assert solve(read_input("data/test_input.txt")) == 157
+def test_part_1_simple():
+    assert solve_priority(read_input("data/test_input.txt")) == 157
 
 
-def test_real():
-    assert solve(read_input("data/input.txt")) == 7990
+def test_part_1_real():
+    assert solve_priority(read_input("data/input.txt")) == 7990
+
+
+def test_part_2_simple():
+    assert solve_badges(read_input("data/test_input.txt")) == 70
+
+
+def test_part_2_real():
+    assert solve_badges(read_input("data/input.txt")) == 2602
 
 
 def read_input(file_name):
     with open(os.path.join(Path(__file__).parent.absolute(), file_name)) as f:
         lines = []
         for line in f:
-            stripped = line.strip('\n')
-            divider = len(stripped) // 2
-            lines.append((stripped[:divider], stripped[divider:]))
+            lines.append(line.strip('\n'))
         return lines
