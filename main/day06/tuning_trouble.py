13fc4208a42bff4875c3bcb51c52fa35b0ec584b
Ben Watson <benwatson528@gmail.com>
2022-12-06 07:33:52 +0000

    Refactoring part 2

--- main/day06/tuning_trouble.py
+++ main/day06/tuning_trouble.py
@@ -1,6 +1,6 @@
 def solve(s, num_unique) -> int:
     for i in range(len(s) - num_unique):
-        deduped = set(list(s[i: i+num_unique]))
+        deduped = set(s[i:i+num_unique])
         if len(deduped) == num_unique:
             return i + num_unique
 
