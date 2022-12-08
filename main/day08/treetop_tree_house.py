6e22dae6f83ab1268a10b8270d3a257a166f3490
Ben Watson <benwatson528@gmail.com>
2022-12-08 08:46:22 +0000

    Day 8

--- main/day08/treetop_tree_house.py
+++ main/day08/treetop_tree_house.py
@@ -1,18 +1,23 @@
-def solve(trees) -> int:
+def solve_visible(trees) -> int:
     num_visible = 0
     for y, _ in enumerate(trees):
         for x, t in enumerate(trees[y]):
-            if is_outer(x, y, trees):
+            if is_outer(x, y, trees) or look_around_visible(x, y, t, trees):
                 num_visible += 1
-            else:
-                around = look_around(x, y, t, trees)
-                num_visible += around
     return num_visible
 
 
-def look_around(x, y, current_tree, trees):
+def solve_scenic(trees) -> int:
+    most_scenic = 0
+    for y, _ in enumerate(trees):
+        for x, t in enumerate(trees[y]):
+            most_scenic = max(look_around_scenic(x, y, t, trees), most_scenic)
+    return most_scenic
+
+
+def look_around_visible(x, y, current_tree, trees):
     taller_seen_left = False
-    for i in range(x):
+    for i in range(x-1, -1, -1):
         if current_tree <= trees[y][i]:
             taller_seen_left = True
     if not taller_seen_left:
@@ -26,7 +31,7 @@ def look_around(x, y, current_tree, trees):
         return True
 
     taller_seen_up = False
-    for j in range(y):
+    for j in range(y-1, -1, -1):
         if current_tree <= trees[j][x]:
             taller_seen_up = True
     if not taller_seen_up:
@@ -39,6 +44,43 @@ def look_around(x, y, current_tree, trees):
     if not taller_seen_down:
         return True
     return False
+
+
+def look_around_scenic(x, y, current_tree, trees):
+    if x == 2 and y == 3:
+        print()
+    num_seen_left = 0
+    for i in range(x-1, -1, -1):
+        if current_tree > trees[y][i]:
+            num_seen_left += 1
+        elif current_tree <= trees[y][i]:
+            num_seen_left += 1
+            break
+
+    num_seen_right = 0
+    for i in range(x+1, len(trees[y])):
+        if current_tree > trees[y][i]:
+            num_seen_right += 1
+        elif current_tree <= trees[y][i]:
+            num_seen_right += 1
+            break
+
+    num_seen_up = 0
+    for j in range(y-1, -1, -1):
+        if current_tree > trees[j][x]:
+            num_seen_up += 1
+        elif current_tree <= trees[j][x]:
+            num_seen_up += 1
+            break
+
+    num_seen_down = 0
+    for j in range(y+1, len(trees)):
+        if current_tree > trees[j][x]:
+            num_seen_down += 1
+        elif current_tree <= trees[j][x]:
+            num_seen_down += 1
+            break
+    return num_seen_left * num_seen_right * num_seen_up * num_seen_down
 
 
 def is_outer(x, y, trees):
--- tests/day08/test_treetop_tree_house.py
+++ tests/day08/test_treetop_tree_house.py
@@ -1,15 +1,23 @@
 import os
 from pathlib import Path
 
-from main.day08.treetop_tree_house import solve
+from main.day08.treetop_tree_house import solve_visible, solve_scenic
 
 
-def test_simple():
-    assert solve(read_input("data/test_input.txt")) == 21
+def test_visible_simple():
+    assert solve_visible(read_input("data/test_input.txt")) == 21
 
 
-def test_real():
-    assert solve(read_input("data/input.txt")) == 1825
+def test_visible_real():
+    assert solve_visible(read_input("data/input.txt")) == 1825
+
+
+def test_scenic_simple():
+    assert solve_scenic(read_input("data/test_input.txt")) == 8
+
+
+def test_scenic_real():
+    assert solve_scenic(read_input("data/input.txt")) == 235200
 
 
 def read_input(file_name):
