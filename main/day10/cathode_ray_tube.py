4345bd880f6844bd25ccbcbc46bd5d4941d4ff42
Ben Watson <benwatson528@gmail.com>
2022-12-10 08:39:00 +0000

    Day 10 part 2

--- main/day10/cathode_ray_tube.py
+++ main/day10/cathode_ray_tube.py
@@ -1,4 +1,4 @@
-def solve(commands) -> int:
+def solve_cycle(commands) -> int:
     signal_strength = 0
     cycle = 0
     x = 1
@@ -16,6 +16,26 @@ def solve(commands) -> int:
     return signal_strength
 
 
+def solve_draw(commands) -> list[str]:
+    grid = []
+    cycle = 0
+    x = 1
+    for cmd in commands:
+        if cmd == "noop":
+            grid.append(draw_pixel(cycle, x))
+            cycle += 1
+        else:
+            grid.append(draw_pixel(cycle, x))
+            cycle += 1
+            grid.append(draw_pixel(cycle, x))
+            cycle += 1
+            x += int(cmd.split(" ")[1])
+    return [''.join(grid[idx:idx + 40]) for idx in range(0, len(grid), 40)]
+
+
+def draw_pixel(cycle, x):
+    return '#' if cycle % 40 in range(x - 1, x + 2) else '.'
+
+
 def check_cycle(cycle, x):
-    if cycle in (20, 60, 100, 140, 180, 220):
-        return cycle * x
+    return cycle * x if cycle in (20, 60, 100, 140, 180, 220) else 0
--- tests/day10/test_cathode_ray_tube.py
+++ tests/day10/test_cathode_ray_tube.py
@@ -1,15 +1,23 @@
 import os
 from pathlib import Path
 
-from main.day10.cathode_ray_tube import solve
+from main.day10.cathode_ray_tube import solve_cycle, solve_draw
 
 
-def test_simple():
-    assert solve(read_input("data/test_input.txt")) == 13140
+def test_cycle_simple():
+    assert solve_cycle(read_input("data/test_input.txt")) == 13140
 
 
-def test_real():
-    assert solve(read_input("data/input.txt")) == 11820
+def test_cycle_real():
+    assert solve_cycle(read_input("data/input.txt")) == 11820
+
+
+def test_draw_simple():
+    assert solve_draw(read_input("data/test_input.txt")) == read_input("data/test_output.txt")
+
+
+def test_draw_real():
+    assert solve_draw(read_input("data/input.txt")) == read_input("data/real_output.txt")  # EPJBRKAH
 
 
 def read_input(file_name):
