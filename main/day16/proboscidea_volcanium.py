5ea65fe8fc12d343f028f3deb4a762df45e258b9
Ben Watson <benwatson528@gmail.com>
2022-12-16 18:00:34 +0000

    Day 16 part 1 test case working

--- main/day16/proboscidea_volcanium.py
+++ main/day16/proboscidea_volcanium.py
@@ -2,37 +2,31 @@ from functools import cache
 
 
 def solve(valves, num_turns) -> int:
-    max_flow_rate = traverse(valves, get_valve("AA", valves), num_turns, 0,
-                             frozenset())
-    return max_flow_rate
+    return traverse(valves, get_valve("AA", valves), num_turns, 0, frozenset(), 0)
 
 
 @cache
-def traverse(valves, current, num_turns, current_turn, open_valves):
-    print(f"current turn = {current_turn}, open_valves = {open_valves}")
-    if current_turn >= 30:
-        return calculate_flow_rate(valves, open_valves, num_turns)
-    max_path = 0
-    for i in range(current_turn, num_turns):
-        if current is None:
-            return calculate_flow_rate(valves, open_valves, num_turns)
-        for neighbour in current.children:
-            if current.id not in [v[0] for v in open_valves]:
-                union = open_valves.union(((current.id, i + 1), ))
-                max_path = max(traverse(valves, get_valve(neighbour, valves),
-                                        num_turns, i + 2, union), max_path)
-            max_path = max(
-                traverse(valves, get_valve(neighbour, valves), num_turns, i + 1,
-                         open_valves), max_path)
-    return calculate_flow_rate(valves, open_valves, num_turns)
+def traverse(valves, current, num_turns, current_turn, open_valves, flow_rate):
+    # print(f"current turn = {current_turn}, open_valves = {open_valves}")
+    if current_turn >= 30 or current is None:
+        return flow_rate
+    max_flow_rate = 0
+    if current.id != "AA" and current.id not in open_valves:
+        # stay where you are and open the valve, costing one move. Don't count the new valve yet
+        max_flow_rate = max(
+            traverse(valves, current, num_turns, current_turn + 1, open_valves.union((current.id,)),
+                     calc_flow_rate(valves, open_valves, flow_rate)), max_flow_rate)
+    for neighbour in current.children:
+        neighbour_valve = get_valve(neighbour, valves)
+        max_flow_rate = max(
+            traverse(valves, neighbour_valve, num_turns, current_turn + 1, open_valves,
+                     calc_flow_rate(valves, open_valves, flow_rate)), max_flow_rate)
+    return max_flow_rate
 
 
-def calculate_flow_rate(graph, open_valves, num_turns):
-    flow_rate = 0
+def calc_flow_rate(valves, open_valves, flow_rate):
     for open_valve in open_valves:
-        valve_id, turn_opened = open_valve[0], open_valve[1]
-        valve_rate = get_valve(valve_id, graph).flow_rate
-        flow_rate += valve_rate * (num_turns - turn_opened)
+        flow_rate += get_valve(open_valve, valves).flow_rate
     return flow_rate
 
 
