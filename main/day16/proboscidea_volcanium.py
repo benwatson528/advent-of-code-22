7bc5782b2568c204dbf3b8ef9de16a5c48dc0d07
Ben Watson <benwatson528@gmail.com>
2022-12-16 18:10:31 +0000

    Day 16 part 1 test case in 30s

--- main/day16/proboscidea_volcanium.py
+++ main/day16/proboscidea_volcanium.py
@@ -1,36 +1,33 @@
 from functools import cache
 
+valves = {}
 
-def solve(valves, num_turns) -> int:
-    return traverse(valves, get_valve("AA", valves), num_turns, 0, frozenset(), 0)
+
+def solve(invalves, num_turns) -> int:
+    global valves
+    valves = invalves
+    return traverse(valves["AA"], num_turns, 0, frozenset(), 0)
 
 
 @cache
-def traverse(valves, current, num_turns, current_turn, open_valves, flow_rate):
-    # print(f"current turn = {current_turn}, open_valves = {open_valves}")
+def traverse(current, num_turns, current_turn, open_valves, flow_rate):
     if current_turn >= 30 or current is None:
         return flow_rate
     max_flow_rate = 0
     if current.id != "AA" and current.id not in open_valves:
         # stay where you are and open the valve, costing one move. Don't count the new valve yet
         max_flow_rate = max(
-            traverse(valves, current, num_turns, current_turn + 1, open_valves.union((current.id,)),
-                     calc_flow_rate(valves, open_valves, flow_rate)), max_flow_rate)
+            traverse(current, num_turns, current_turn + 1, open_valves.union((current.id,)),
+                     calc_flow_rate(open_valves, flow_rate)), max_flow_rate)
     for neighbour in current.children:
-        neighbour_valve = get_valve(neighbour, valves)
+        neighbour_valve = valves[neighbour]
         max_flow_rate = max(
-            traverse(valves, neighbour_valve, num_turns, current_turn + 1, open_valves,
-                     calc_flow_rate(valves, open_valves, flow_rate)), max_flow_rate)
+            traverse(neighbour_valve, num_turns, current_turn + 1, open_valves,
+                     calc_flow_rate(open_valves, flow_rate)), max_flow_rate)
     return max_flow_rate
 
 
-def calc_flow_rate(valves, open_valves, flow_rate):
+def calc_flow_rate(open_valves, flow_rate):
     for open_valve in open_valves:
-        flow_rate += get_valve(open_valve, valves).flow_rate
+        flow_rate += valves[open_valve].flow_rate
     return flow_rate
-
-
-def get_valve(valve_id, valves):
-    for valve in valves:
-        if valve_id == valve.id:
-            return valve
--- tests/day16/test_proboscidea_volcanium.py
+++ tests/day16/test_proboscidea_volcanium.py
@@ -16,10 +16,10 @@ def test_real():
 
 def read_input(file_name):
     with open(os.path.join(Path(__file__).parent.absolute(), file_name)) as f:
-        valves = set()
+        valves = {}
         for line in f:
             matches = re.findall(r"Valve ([A-Z]+) has flow rate=(\d+); "
                                  r"tunnels? leads? to valves? (.+)", line)[0]
-            valves.add(Valve(matches[0], int(matches[1]),
-                             frozenset(matches[2].split(", "))))
-        return frozenset(valves)
+            valves[matches[0]] = Valve(matches[0], int(matches[1]),
+                                       frozenset(matches[2].split(", ")))
+        return valves
