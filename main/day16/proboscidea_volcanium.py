22d057e1712a23f4f61d92fc3f68c218ac689b29
Ben Watson <benwatson528@gmail.com>
2022-12-16 18:18:48 +0000

    Day 16 part 1 test case in 3s

--- main/day16/proboscidea_volcanium.py
+++ main/day16/proboscidea_volcanium.py
@@ -1,11 +1,14 @@
 from functools import cache
 
 valves = {}
+positive_flow_rates = set()
 
 
 def solve(invalves, num_turns) -> int:
     global valves
     valves = invalves
+    global positive_flow_rates
+    positive_flow_rates = set(valve.id for valve in valves.values() if valve.flow_rate != 0)
     return traverse(valves["AA"], num_turns, 0, frozenset(), 0)
 
 
@@ -14,15 +17,14 @@ def traverse(current, num_turns, current_turn, open_valves, flow_rate):
     if current_turn >= 30 or current is None:
         return flow_rate
     max_flow_rate = 0
-    if current.id != "AA" and current.id not in open_valves:
+    if current.id in positive_flow_rates and current.id not in open_valves:
         # stay where you are and open the valve, costing one move. Don't count the new valve yet
         max_flow_rate = max(
             traverse(current, num_turns, current_turn + 1, open_valves.union((current.id,)),
                      calc_flow_rate(open_valves, flow_rate)), max_flow_rate)
     for neighbour in current.children:
-        neighbour_valve = valves[neighbour]
         max_flow_rate = max(
-            traverse(neighbour_valve, num_turns, current_turn + 1, open_valves,
+            traverse(valves[neighbour], num_turns, current_turn + 1, open_valves,
                      calc_flow_rate(open_valves, flow_rate)), max_flow_rate)
     return max_flow_rate
 
