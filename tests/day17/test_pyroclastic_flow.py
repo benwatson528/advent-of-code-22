baec576664b56e469f2600e9cb0cef66d04366d1
Ben Watson <benwatson528@gmail.com>
2022-12-17 15:48:08 +0000

    Day 17 part 1 cleanup

--- tests/day17/test_pyroclastic_flow.py
+++ tests/day17/test_pyroclastic_flow.py
@@ -4,12 +4,21 @@ from pathlib import Path
 from main.day17.pyroclastic_flow import solve
 
 
-def test_simple():
+def test_p1_simple():
     assert solve(read_jet_pattern("data/test_input.txt"), read_shapes("data/blocks.txt"), 2022) == 3068
 
 
-def test_real():
+def test_p1_real():
     assert solve(read_jet_pattern("data/input.txt"), read_shapes("data/blocks.txt"), 2022) == 3219
+
+
+def test_p2_simple():
+    assert solve(read_jet_pattern("data/test_input.txt"), read_shapes("data/blocks.txt"),
+                 1000000000000) == 1514285714288
+
+
+def test_p2_real():
+    assert solve(read_jet_pattern("data/input.txt"), read_shapes("data/blocks.txt"), 1000000000000) == -1
 
 
 def read_jet_pattern(file_name):
@@ -22,7 +31,7 @@ def read_shapes(file_name):
         shapes = []
         for raw_block in f.read().split("\n\n"):
             shape = set()
-            split_raw_block = raw_block.split("\n")
+            split_raw_block = [raw for raw in raw_block.split("\n") if len(raw) != 0]
             shape_height = len(split_raw_block)
             for j in range(len(split_raw_block)):
                 for i in range(len(split_raw_block[j])):
