de10366e3472c3db49adcb183baa84392547fbd0
Ben Watson <benwatson528@gmail.com>
2022-12-18 16:21:43 +0000

    Day 18 both parts

--- main/day18/boiling_boulders.py
+++ main/day18/boiling_boulders.py
@@ -1,8 +1,43 @@
-def solve(cubes) -> int:
-    x_z_faces = get_x_z_faces(cubes)
-    x_y_faces = get_x_y_faces(cubes)
-    y_z_faces = get_y_z_faces(cubes)
-    return len(x_z_faces) + len(x_y_faces) + len(y_z_faces)
+def solve_all_surface_area(cubes) -> int:
+    return len(get_x_z_faces(cubes)) + len(get_x_y_faces(cubes)) + len(get_y_z_faces(cubes))
+
+
+# This is quite inefficient. It would be better to flood fill the outside and then count the flooded boundaries.
+def solve_outer_surface_area(cubes) -> int:
+    bounds = (min(x for (x, y, z) in cubes), max(x for (x, y, z) in cubes),
+              min(y for (x, y, z) in cubes), max(y for (x, y, z) in cubes),
+              min(z for (x, y, z) in cubes), max(z for (x, y, z) in cubes))
+    surface_area = 0
+    for cube in cubes:
+        for neighbour in get_neighbours(cube):
+            if neighbour not in cubes and dfs(neighbour, cubes, bounds):
+                surface_area += 1
+    return surface_area
+
+
+def dfs(cube, cubes, bounds):
+    visited = set()
+    to_visit = [cube]
+    while to_visit:
+        current = to_visit.pop()
+        if current not in cubes and current not in visited:
+            visited.add(current)
+            if has_escaped(current, bounds):
+                return True
+            to_visit.extend(get_neighbours(current))
+    return False
+
+
+def get_neighbours(cube):
+    return [(cube[0] - 1, cube[1], cube[2]), (cube[0] + 1, cube[1], cube[2]),
+            (cube[0], cube[1] - 1, cube[2]), (cube[0], cube[1] + 1, cube[2]),
+            (cube[0], cube[1], cube[2] - 1), (cube[0], cube[1], cube[2] + 1)]
+
+
+def has_escaped(cube, bounds):
+    return cube[0] < bounds[0] or cube[0] > bounds[1] or \
+        cube[1] < bounds[2] or cube[1] > bounds[3] or \
+        cube[2] < bounds[4] or cube[2] > bounds[5]
 
 
 def get_x_z_faces(cubes):
--- tests/day18/test_boiling_boulders.py
+++ tests/day18/test_boiling_boulders.py
@@ -1,24 +1,30 @@
 import os
 from pathlib import Path
 
-from main.day18.boiling_boulders import solve
+import pytest
+
+from main.day18.boiling_boulders import solve_all_surface_area, solve_outer_surface_area
 
 
-def test_small_simple():
-    assert solve(read_input("data/small_test_input.txt")) == 10
+def test_all_simple():
+    assert solve_all_surface_area(read_input("data/test_input.txt")) == 64
 
 
-def test_simple():
-    assert solve(read_input("data/test_input.txt")) == 64
+def test_all_real():
+    assert solve_all_surface_area(read_input("data/input.txt")) == 3454
 
 
-def test_real():
-    assert solve(read_input("data/input.txt")) == 3454
+def test_inner_simple():
+    assert solve_outer_surface_area(read_input("data/test_input.txt")) == 58
+
+
+def test_inner_real():
+    assert solve_outer_surface_area(read_input("data/input.txt")) == 2014
 
 
 def read_input(file_name):
     with open(os.path.join(Path(__file__).parent.absolute(), file_name)) as f:
-        lines = []
+        lines = set()
         for line in f:
-            lines.append(list(int(x) for x in line.strip('\n').split(",")))
+            lines.add(tuple((int(x) for x in line.strip('\n').split(","))))
         return lines
