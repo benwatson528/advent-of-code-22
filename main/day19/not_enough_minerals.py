7987d5e65c849210203ed289af7d8b5f04ffb913
Ben Watson <benwatson528@gmail.com>
2022-12-19 22:14:30 +0000

    Day 19 not working

--- main/day19/not_enough_minerals.py
+++ main/day19/not_enough_minerals.py
@@ -1,48 +1,77 @@
 import copy
+from functools import cache
+
+COST = {}
+NUM_TURNS = 0
+MAPPING = {"ore": 0, "clay": 1, "obsidian": 2, "geode": 3}
 
 
 def solve(blueprints, num_turns) -> int:
-    output_blueprints = []
-    for blueprint in blueprints:
-        output_blueprints.append(run_blueprint(blueprint, 0, num_turns))
-        print(f"Processed blueprint {blueprint}")
+    robots = (1, 0, 0, 0)
+    elements = (0, 0, 0, 0)
+    output_geodes = []
+    for i, cost in enumerate(blueprints):
+        global COST
+        COST = cost
+        global NUM_TURNS
+        NUM_TURNS = num_turns
+        output_geodes.append(run_blueprint(robots, elements, i + 1, 1))
 
-    return calculate_quality_level(output_blueprints)
-
-
-def run_blueprint(blueprint, current_turn, num_turns):
-    max_geodes_mined_blueprint = blueprint
-    if current_turn < num_turns:
-        blueprint.mine()
-        # mine each element that we could mine
-        for robot in blueprint.robots.keys():
-            if blueprint.can_create_robot(robot):
-                blueprint_copy = copy.deepcopy(blueprint)
-                blueprint_copy.create_robot(robot)
-                max_geodes_mined_blueprint = get_blueprint_with_max_geode(
-                    run_blueprint(blueprint_copy, current_turn + 1, num_turns),
-                    max_geodes_mined_blueprint)
-
-        # do nothing
-        blueprint_copy = copy.deepcopy(blueprint)
-        max_geodes_mined_blueprint = get_blueprint_with_max_geode(
-            run_blueprint(blueprint_copy, current_turn + 1, num_turns),
-            max_geodes_mined_blueprint)
-    return max_geodes_mined_blueprint
+    return calculate_quality_level(output_geodes)
 
 
-def get_blueprint_with_max_geode(blueprint, max_geodes_mined_blueprint):
-    if blueprint.elements["geode"] > max_geodes_mined_blueprint.elements[
-        "geode"]:
-        return blueprint
+@cache
+def run_blueprint(robots, elements, id, current_turn):
+    if current_turn > NUM_TURNS:
+        return elements[3]
+    max_num_geodes = 0
+    next_paths = []
+    if can_create_robot("geode", elements):
+        next_paths.append(create_robot("geode", robots, elements))
     else:
-        return max_geodes_mined_blueprint
+        if can_create_robot("obsidian", elements) and robots[2] < 21 and elements[2] < 21:
+            next_paths.append(create_robot("obsidian", robots, elements))
+        if can_create_robot("clay", elements) and robots[1] < 21 and elements[1] < 21:
+            next_paths.append(create_robot("clay", robots, elements))
+        if can_create_robot("ore", elements) and robots[0] < 5 and elements[0] < 5:
+            next_paths.append(create_robot("ore", robots, elements))
+        next_paths.append((robots, elements))
+
+    for next_robot, next_elements in next_paths:
+        max_num_geodes = max(run_blueprint(next_robot, mine(robots, next_elements), id, current_turn + 1),
+                             max_num_geodes)
+
+    return max_num_geodes
 
 
-def calculate_quality_level(blueprints):
+def mine(robots, elements):
+    new_elements = list(elements)
+    for robot_type, num_of_robot in enumerate(robots):
+        new_elements[robot_type] += num_of_robot
+    return tuple(new_elements)
+
+
+def can_create_robot(robot_type, elements):
+    for element, cost in COST[robot_type].items():
+        if elements[MAPPING[element]] < cost:
+            return False
+    return True
+
+
+def create_robot(robot_type, robots, elements):
+    robots_idx = MAPPING[robot_type]
+    new_robots = list(robots)
+    new_robots[robots_idx] = robots[robots_idx] + 1
+    new_elements = list(elements)
+    for element_type, element_cost in COST[robot_type].items():
+        new_elements[MAPPING[element_type]] -= element_cost
+    return tuple(new_robots), tuple(new_elements)
+
+
+def calculate_quality_level(geodes):
     print()
     quality_level = 0
-    for blueprint in blueprints:
-        print(f"final blueprint = {blueprint}")
-        quality_level += (blueprint.id * blueprint.elements["geode"])
+    for i, geode in enumerate(geodes):
+        print(f"blueprint {i + 1} has {geode} geodes")
+        quality_level += ((i + 1) * geode)
     return quality_level
--- tests/day19/test_not_enough_minerals.py
+++ tests/day19/test_not_enough_minerals.py
@@ -2,29 +2,28 @@ import os
 import re
 from pathlib import Path
 
-from main.day19.Blueprint import Blueprint
+import pytest
+
 from main.day19.not_enough_minerals import solve
 
 
+@pytest.mark.skip(reason="Very slow")
 def test_simple():
-    assert solve(read_input("data/test_input.txt"), 15) == 33
+    assert solve(read_input("data/test_input.txt"), 24) == 33
 
 
-def test_real():
-    assert solve(read_input("data/input.txt"), 24) == 0
+# def test_real():
+#     assert solve(read_input("data/input.txt"), 24) == 0  # 1710 too high
 
 
 def read_input(file_name):
     with open(os.path.join(Path(__file__).parent.absolute(), file_name)) as f:
-        blueprints = []
+        costs = []
         for line in f.readlines():
             nums = [int(x) for x in re.findall(r'\d+', line)]
-            robots = {"ore": 1, "clay": 0, "obsidian": 0, "geode": 0}
-            elements = {"ore": 0, "clay": 0, "obsidian": 0, "geode": 0}
-            costs = {
+            costs.append({
                 "geode": {"ore": nums[5], "obsidian": nums[6]},
                 "obsidian": {"clay": nums[4], "ore": nums[3]},
                 "clay": {"ore": nums[2]},
-                "ore": {"ore": nums[1]}}
-            blueprints.append(Blueprint(nums[0], robots, elements, costs))
-        return blueprints
+                "ore": {"ore": nums[1]}})
+        return costs
