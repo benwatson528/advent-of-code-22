8ec34f8ee8bba596e42e7f66fc1f9adb523dfbe1
Ben Watson <benwatson528@gmail.com>
2022-12-24 17:21:56 +0000

    Day 24 part 1

--- main/day24/blizzard_basin.py
+++ main/day24/blizzard_basin.py
@@ -1,56 +1,83 @@
+from collections import deque
+from functools import cmp_to_key
+
+
 def solve(start, end, blizzards) -> int:
     return dfs(start, end, blizzards)
 
 
 def dfs(start, end, blizzards):
-    # height and width include the outer walls
     grid_height, grid_width = end[0], end[1] + 2
-    to_visit = [(start, 0)]
-    fastest_exit = 9999
+    to_visit = deque()
+    to_visit.append((start, 0))
+    fastest_exit = 861
+    dead_ends = set()
+    i = 0
     while to_visit:
-        current_position, current_minute = to_visit.pop()
-        if current_position == (1, 1):
-            print()
-        if current_minute > fastest_exit:
-            continue
+        i += 1
+        current_position, current_minute = to_visit.popleft()
         if current_position == end:
             fastest_exit = min(fastest_exit, current_minute)
             continue
-        if is_in_blizzard(current_position, current_minute, blizzards, (grid_height, grid_width)):
+        if (current_position, current_minute) in dead_ends:
             continue
-        for next_position in get_next_moves(current_position, end, grid_height, grid_width):
+        if current_minute > fastest_exit:
+            continue
+        if is_in_blizzard(current_position, current_minute, blizzards, (grid_height, grid_width)):
+            dead_ends.add((current_position, current_minute))
+            continue
+        for next_position in get_next_moves(current_position, start, end, grid_height, grid_width):
             to_visit.append((next_position, current_minute + 1))
+        if i % 100 == 0:
+            to_visit = deque(dict.fromkeys(to_visit))
     return fastest_exit
 
 
-# still need to move the left and up in negative directions...
-def is_in_blizzard(current_position, current_minute, blizzards, grid_size):
-    for up_blizzard in blizzards["^"][current_position[1]]:
-        if (up_blizzard - current_minute - 1) % (grid_size[0] - 1) + 1 == current_position[0]:
-            return True
-    for down_blizzard in blizzards["v"][current_position[1]]:
-        if (down_blizzard + current_minute - 1) % (grid_size[0] - 1) + 1 == current_position[0]:
-            return True
-    for left_blizzard in blizzards["<"][current_position[0]]:
-        if (left_blizzard - current_minute - 1) % (grid_size[1] - 2) + 1 == current_position[1]:
-            return True
-    for right_blizzard in blizzards[">"][current_position[0]]:
-        if (right_blizzard + current_minute - 1) % (grid_size[1] - 2) + 1 == current_position[1]:
+def is_in_blizzard(current_position, minute, blizzards, grid_size):
+    if minute not in blizzards["^"]:
+        evolve_blizzards(blizzards, minute - 1, grid_size)
+    for direction, times in blizzards.items():
+        if current_position in times[minute]:
             return True
     return False
 
 
-def get_next_moves(current_position, end, grid_height, grid_width):
-    return [position for position in [(current_position[0] - 1, current_position[1]),
-                                      (current_position[0], current_position[1] - 1),
+def evolve_blizzards(blizzards, previous_minute, grid_size):
+    for direction, times in blizzards.items():
+        for coord in times[previous_minute]:
+            blizzards[direction][previous_minute + 1].add(move_coord(coord, direction, grid_size))
+
+
+def move_coord(coord, direction, grid_size):
+    match direction:
+        case "^":
+            czord = (grid_size[0] - 1, coord[1]) if coord[0] - 1 == 0 else (coord[0] - 1, coord[1])
+            return czord
+        case "v":
+            czord = (1, coord[1]) if coord[0] + 1 == grid_size[0] else (coord[0] + 1, coord[1])
+            return czord
+        case "<":
+            czord = (coord[0], grid_size[1] - 2) if coord[1] - 1 == 0 else (coord[0], coord[1] - 1)
+            return czord
+        case ">":
+            czord = (coord[0], 1) if coord[1] + 1 == grid_size[1] - 1 else (coord[0], coord[1] + 1)
+            return czord
+
+
+def get_next_moves(current_position, start, end, grid_height, grid_width):
+    return [position for position in [(current_position[0] + 1, current_position[1]),
+                                      (current_position[0], current_position[1] + 1),
                                       current_position,
-                                      (current_position[0] + 1, current_position[1]),
-                                      (current_position[0], current_position[1] + 1)]
-            if is_valid_move(position, end, grid_height, grid_width)]
+                                      (current_position[0] - 1, current_position[1]),
+                                      (current_position[0], current_position[1] - 1)
+                                      ]
+            if is_valid_move(position, start, end, grid_height, grid_width)]
 
 
-def is_valid_move(position, end, grid_height, grid_width):
+def is_valid_move(position, start, end, grid_height, grid_width):
     if position == end:
+        return True
+    if position == start:
         return True
     if 0 < position[0] < grid_height and 0 < position[1] < grid_width - 1:
         return True
--- tests/day24/test_blizzard_basin.py
+++ tests/day24/test_blizzard_basin.py
@@ -12,32 +12,20 @@ def test_simple():
 
 def test_real():
     start, end, blizzards = read_input("data/input.txt")
-    assert solve(start, end, blizzards) == 0
+    assert solve(start, end, blizzards) == 326
 
 
 def read_input(file_name):
     with open(os.path.join(Path(__file__).parent.absolute(), file_name)) as f:
         lines = f.read().splitlines()
-        blizzards = {"^": defaultdict(list), "v": defaultdict(list), "<": defaultdict(list), ">": defaultdict(list)}
+        blizzards = {"^": defaultdict(set), "v": defaultdict(set), "<": defaultdict(set), ">": defaultdict(set)}
         for r in range(len(lines)):
             for c in range(len(lines[r])):
                 if lines[r][c] in ("v", "<", ">", "^"):
-                    add_blizzard(blizzards, r, c, lines[r][c])
-                if r == 0 and lines[r][c] == ".":
+                    # direction -> minute -> set[coords]
+                    blizzards[lines[r][c]][0].add((r, c))
+                elif r == 0 and lines[r][c] == ".":
                     start = (r, c)
                 elif r == len(lines) - 1 and lines[r][c] == ".":
                     end = (r, c)
         return start, end, blizzards
-
-
-# blizzards_0 = up, blizzards_1 = down, blizzards_2 = left, blizzards_3 = right
-def add_blizzard(blizzards, r, c, direction):
-    match direction:
-        case "^":
-            blizzards["^"][c].append(r)
-        case "v":
-            blizzards["v"][c].append(r)
-        case "<":
-            blizzards["<"][r].append(c)
-        case ">":
-            blizzards[">"][r].append(c)
